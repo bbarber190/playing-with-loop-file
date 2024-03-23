015f9b083c89e87ed681fb730ab6c82ac5b6e61b
bbarber190 <53317278+bbarber190@users.noreply.github.com>
2024-03-23 12:23:04 -0400

    Create for.py
    playing with the for loop!

---
+++ for.py
@@ -0,0 +1,15 @@
+#knowing the for statement by Brandon Barber
+###   3.23.24
+phrase = "bolwing up python  oo ooo!"
+
+for i in phrase:
+  print (phrase)
+
+## or...
+
+for i in range (7):
+  print(phrase)
+## and...
+
+for i in range (7):
+  print(i)
